03ea7fe7866654ba64c70aca3ee60a1571ac5cd9
Maxwell <Maxwell.Keely@epa.gov>
2016-09-16 15:13:31 -0400

    We have a graph

--- ToDoScript.R
+++ ToDoScript.R
@@ -4,22 +4,55 @@
 ##first add new colum in excel (threshold score/IV)
 install.packages("xlsx")
 library(xlsx)
-CERIData <-read.xlsx("SESYNCTrainingQuantitative.xlsx",
-    sheetIndex=1,
-    na.strings= c(""))
 
+CERIData2 <-read_excel("SESYNCTrainingQuantitative.xlsx",
+    sheet=1,
+    na= c("No threshold", "No indicator value",
+      "No threshold or indicator value","Score not yet assigned",
+      "to be assigned", "Not relevant", "<NA>", "N/A"))
+##NA strings doesn't work
+install.packages("readxl")
+library(readxl)
 
+#column comparison
+
+#machine 1
+library(dplyr)
+Threshold_Score_Col <- select(CERIData, Threshold.Score)
+save(Threshold_Score_Col, file= "Threshold_Score_Col.Rdata")
+
+#machine 2
+load("Threshold_Score_Col.Rdata")
+library(dplyr)
+select(CERIData, Threshold.Score)== Threshold_Score_Col
+st
 str(CERIData) 
 as.factor(...levels...)
 rename?columns
 
 ##subset/group/filter/aggregate
 library(dplyr)
-
+#Threshold.score has: No threshold, No indicator value, No threshold or indicator value
+#Participant.Score..1.4. has: Score not yet assigned, to be assigned, Not relevant, <NA>, N/A
+SelectCERIData <- select(CERIData, NA..4, Threshold.Score, Participant.Score..1.4.)
+SELECTCERIDATA_NA <- 
+#use in operator to find string and convert to NA
+SelectCERIData$Threshold.Score[CERIData$Threshold.Score== "No threshold"]<- "NA"
+SelectCERIData$Threshold.Score[CERIData$Threshold.Score== "No indicator value"]<- "NA"
+SelectCERIData$Threshold.Score[CERIData$Threshold.Score== "No threshold or indicator value"]<- "NA"
+SelectCERIData$Participant.Score..1.4.[CERIData$Participant.Score..1.4.=="Score not yet assigned"]<- "NA"
+SelectCERIData$Participant.Score..1.4.[CERIData$Participant.Score..1.4.=="to be assigned"]<- "NA"
+SelectCERIData$Participant.Score..1.4.[CERIData$Participant.Score..1.4.=="Not relevant"]<- "NA"
+SelectCERIData$Participant.Score..1.4.[CERIData$Participant.Score..1.4.=="<NA>"]<- "NA"
+SelectCERIData$Participant.Score..1.4.[CERIData$Participant.Score..1.4.=="N/A"]<- "NA"
+SelectCERIData_NA <- subset(SelectCERIData, 
+##nope
 
 #plot
 library(ggplot2)
-CERIPlot1 <- ggplot(data=CERIData, 
-    aes(x=Threshold_Score, y = Participant_Score)
+CERIPlot1 <- ggplot(data = SelectCERIData, 
+    aes(x = Threshold.Score, 
+      y = Participant.Score..1.4., color= NA..4))+
     geom_point()
-    )
+
+##errors
